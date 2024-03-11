ce9ce8ed37f550bcc15cf04f5c84dea3a03acb57
Gustavo <gustavo.fonseca@cleversystems.com.br>
2024-03-11 17:27:29 -0300

    primeiro commit

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: [
+    "./node_modules/appcenter",
+    "./node_modules/appcenter-analytics",
+    "./node_modules/appcenter-crashes",
+    "./node_modules/react-native-code-push",
+  ],
+};
